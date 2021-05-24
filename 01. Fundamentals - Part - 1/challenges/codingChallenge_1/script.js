4e011bdbd84665fcfacbbd6cdbbb4b217ee22496
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-05-24 18:30:03 +0300

    coding challenge one

---
+++ 01. Fundamentals - Part - 1/challenges/codingChallenge_1/script.js
@@ -0,0 +1,29 @@
+/*
+    Mark and John are trying to compare their BMI (Body Mass Index), which is
+    calculated using the formula:
+    BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+    and height in meter).
+    Your tasks:
+        1. Store Mark's and John's mass and height in variables
+        2. Calculate both their BMIs using the formula (you can even implement both versions)
+        3. Create a Boolean variable 'markHigherBMI' containing information about
+            whether Mark has a higher BMI than John.
+    Test data:
+        § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+        § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/
+
+function solve() {
+    function caclulateBMI(mass, height) {
+        return mass / height ** 2;
+    }
+
+    let markHigherBMI = caclulateBMI(95, 1.88);
+    let johnHigherBMI = caclulateBMI(85, 1.76);
+
+    let isTrue = markHigherBMI > johnHigherBMI;
+
+    console.log(isTrue);
+}
+
+solve();
--- 01. Fundamentals - Part - 1/script.js
+++ 01. Fundamentals - Part - 1/script.js
@@ -1,7 +1,32 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// Values and Variables
+/*
+let country = 'Bulgaria';
+let continent = 'Europe';
+let population = 80000000;
 
-console.log(23);
+// console.log(country, continent, population);
 
-let firstName = 'Jonas';
-console.log(firstName);
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
+
+const birthYear = 1988;
+
+const now = 2038;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2;
+
+let x = 10 + 5;
+
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100;
+x++; // x = x + 1;
+x--; // x = x - 1;
